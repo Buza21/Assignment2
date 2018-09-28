c633a7df1b388a6ea8062e20c5e8b22fc30aaf51
Trevor Buza <Tbuza@oakland.edu>
2018-09-28 17:02:03 -0400

    Main and Bottles Fn added

--- Assignment2_Trevor_Buza_Git/Assignment2_Trevor_Buza_Git/Form1.vb
+++ Assignment2_Trevor_Buza_Git/Assignment2_Trevor_Buza_Git/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+    Private Sub Main()
 
+    End Sub
+    Private Sub Ninety_Nine_Bottles_of_Beer()
+
+    End Sub
 End Class
